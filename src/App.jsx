572210b22386a71db5f5644d3e0b156f1e7c5d28
GusLV12 <123591605+GusLV12@users.noreply.github.com>
2023-12-18 13:10:00 -0600

    Agregando logica

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 let table = [
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', '']
-]
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
 
 function App() {
+  const [turn, setTurn] = useState("PLAYER");
+
   useEffect(() => {
     const canvas = document.querySelector("canvas");
     const ctx = canvas.getContext("2d");
@@ -18,52 +20,125 @@ function App() {
     drawGame(ctx, anchoCelda, altoCelda);
   }, []);
 
+  useEffect(() => {
+    if(turn === "ROBOT"){
+      const canvas = document.querySelector('canvas');
+      const ctx = canvas.getContext('2d');
+      let ancho = canvas.width;
+      let alto = canvas.height;
+      let anchoCelda = ancho / 3;
+      let altoCelda = alto / 3;
+
+      eventTurnRobot(ctx, anchoCelda, altoCelda);
+    }
+
+  },[turn])
+
+
   const drawLine = (ctx, x1, y1, x2, y2) => {
-    ctx.beginPath()
-    ctx.moveTo(x1,y1);
-    ctx.lineTo(x2,y2);
-    ctx.stroke()
-  }
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+  };
+
   const drawTable = (ctx, ancho, alto, anchoCelda, altoCelda) => {
     ctx.lineWidth = 2;
-    ctx.clearRect(0,0,ancho,alto);
-    ctx.strokeStyle = 'white'
-    drawLine(ctx, 0, altoCelda, ancho, altoCelda)
+    ctx.clearRect(0, 0, ancho, alto);
+    ctx.strokeStyle = "white";
+    drawLine(ctx, 0, altoCelda, ancho, altoCelda);
     drawLine(ctx, 0, altoCelda * 2, ancho, altoCelda * 2);
     drawLine(ctx, anchoCelda, 0, anchoCelda, ancho);
     drawLine(ctx, anchoCelda * 2, 0, anchoCelda * 2, ancho);
-
   };
 
   const drawX = (ctx, x1, y1, x2, y2, x3, y3, x4, y4) => {
     drawLine(ctx, x1, y1, x2, y2);
     drawLine(ctx, x3, y3, x4, y4);
-  }
+  };
 
   const drawO = (ctx, x, y, radio) => {
     ctx.beginPath();
-    ctx.arc(x, y, radio, 0, Math.PI*2)
-    ctx.stroke()
-  }
+    ctx.arc(x, y, radio, 0, Math.PI * 2);
+    ctx.stroke();
+  };
 
   const drawGame = (ctx, anchoCelda, altoCelda) => {
-    for(let i =0; i<3; i++){
-      for(let j=0; j<3; j++){
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
         let x = anchoCelda * i + anchoCelda / 2;
         let y = altoCelda * j + altoCelda / 2;
         let radio = anchoCelda / 3;
-        if(table[i][j] === 'turn.ROBOT'){
-          drawX(ctx, x-radio, y-radio, x+radio, y+radio, x+radio, y-radio, x-radio, y+radio)
-        }else if(table[i][j] === 'turn.PLAYER'){
-          drawO(ctx, x, y, radio)
+        if (table[i][j] === "ROBOT") {
+          drawX(
+            ctx,
+            x - radio,
+            y - radio,
+            x + radio,
+            y + radio,
+            x + radio,
+            y - radio,
+            x - radio,
+            y + radio
+          );
+        } else if (table[i][j] === "PLAYER") {
+          drawO(ctx, x, y, radio);
         }
+      }
+    }
+  };
+
+
+  const eventTurnRobot = (ctx, anchoCelda, altoCelda) => {
+    if (turn === "ROBOT") {
+      let mov = null;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (table[i][j] === '') {
+            mov = { i, j }; // Corregir aquí
+          }
+        }
+      }
+  
+      if (mov !== null) {
+        table[mov.i][mov.j] = "ROBOT";
+        drawGame(ctx, anchoCelda, altoCelda);
+        setTurn("PLAYER");
+      }
+      
+    }
+  }
+
+  const eventDrawTable = (e) => {
+    if (turn === "PLAYER") {
+      const canvas = document.querySelector('canvas');
+      const ctx = canvas.getContext('2d');
+      let ancho = canvas.width;
+      let alto = canvas.height;
+      let anchoCelda = ancho / 3;
+      let altoCelda = alto / 3;
+      const mouseX = e.clientX - canvas.getBoundingClientRect().left;
+      const mouseY = e.clientY - canvas.getBoundingClientRect().top;
+
+      let i = Math.floor(mouseX / anchoCelda);
+      let j = Math.floor(mouseY / altoCelda);
+
+      if (table[i][j] === "") {
+        table[i][j] = "PLAYER";
+        drawGame(ctx, anchoCelda, altoCelda)
+        setTurn("ROBOT");
       }
     }
   }
 
   return (
     <div>
-      <canvas style={{ background: "teal" }} width="400" height="400" />
+      <canvas 
+        style={{ background: "teal" }} 
+        width="400" 
+        height="400" 
+        onClick={eventDrawTable}
+      />
     </div>
   );
 }
